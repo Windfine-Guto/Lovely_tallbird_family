e25fe6d3c310501a3dac1e68a49c8d14240164cc
over-dragon <over_dragon@163.com>
2026-04-05 00:17:23 +0800

    Add files via upload

---
+++ lovely_tallbird_family/string_en.lua
@@ -0,0 +1,168 @@
+STRINGS.NAMES.TALLBIRD_SADDLE = "Tallbird Saddle"
+STRINGS.RECIPE_DESC.TALLBIRD_SADDLE = "Ride it and become elevated一safety not included."
+--角色检查台词
+STRINGS.CHARACTERS.GENERIC.DESCRIBE.TALLBIRD_SADDLE = "Huh, a saddle for a Tallbird. Quite clever design."
+STRINGS.CHARACTERS.WENDY.DESCRIBE.TALLBIRD_SADDLE = "To torment two lives simultaneously."
+STRINGS.CHARACTERS.WINOAN.DESCRIBE.TALLBIRD_SADDLE= "Will this hold that fierce bird? Don’t break mid‑ride."
+STRINGS.CHARACTERS.WOLFGANG.DESCRIBE.TALLBIRD_SADDLE= "The saddle of the big bird... Hope it doesn't drop Wolfgang off!"
+STRINGS.CHARACTERS.WICKERBOTTOM.DESCRIBE.TALLBIRD_SADDLE= "Someone has crafted a saddle specially for the tallbird. Quite the novel invention."
+STRINGS.CHARACTERS.WANDA.DESCRIBE.TALLBIRD_SADDLE= "I don't want to spend time trying to maintain balance on it."
+STRINGS.CHARACTERS.WOODIE.DESCRIBE.TALLBIRD_SADDLE= "What a crazy idea!"
+STRINGS.CHARACTERS.WARLY.DESCRIBE.TALLBIRD_SADDLE= "I hope he doesn't hold a grudge for me cooking his young."
+STRINGS.CHARACTERS.WATHGRITHR.DESCRIBE.TALLBIRD_SADDLE= "Simple saddle, yet it takes Ördinary courage to tame this beast!"
+STRINGS.CHARACTERS.WILLOW.DESCRIBE.TALLBIRD_SADDLE= "Why pick on a one-eyed creature!"
+STRINGS.CHARACTERS.WORMWOOD.DESCRIBE.TALLBIRD_SADDLE= "Big peep-peep chair."
+STRINGS.CHARACTERS.WAXWELL.DESCRIBE.TALLBIRD_SADDLE= "This is simply absurd."
+STRINGS.CHARACTERS.WEBBER.DESCRIBE.TALLBIRD_SADDLE= "We can ride the big bird now!"
+STRINGS.CHARACTERS.WALTER.DESCRIBE.TALLBIRD_SADDLE= "Woby, look! We can ride the big bird!"
+STRINGS.CHARACTERS.WURT.DESCRIBE.TALLBIRD_SADDLE= "It has a big eye painted on it. Spooky."
+STRINGS.CHARACTERS.WX78.DESCRIBE.TALLBIRD_SADDLE= "I will torment you."
+STRINGS.CHARACTERS.WORTOX.DESCRIBE.TALLBIRD_SADDLE= "Hmph! Stop staring at me!"
+
+---English
+---骑乘高脚鸟低生命时的台词 写""里面
+---威尔逊
+STRINGS.CHARACTERS.GENERIC.ANNOUNCE_TALLBIRD_MOUNT_LOWHEALTH = "The bird's not going to make it—I need to heal it"
+---温蒂
+STRINGS.CHARACTERS.WENDY.ANNOUNCE_TALLBIRD_MOUNT_LOWHEALTH = "Both the bird and my heart are drawing near to death."
+---薇诺娜
+STRINGS.CHARACTERS.WINONA.ANNOUNCE_TALLBIRD_MOUNT_LOWHEALTH= "I ought to keep an eye on the bird's health."
+---沃尔夫冈
+STRINGS.CHARACTERS.WOLFGANG.ANNOUNCE_TALLBIRD_MOUNT_LOWHEALTH= "The big bird seems badly hurt."
+---薇克巴顿
+STRINGS.CHARACTERS.WICKERBOTTOM.ANNOUNCE_TALLBIRD_MOUNT_LOWHEALTH= "The bird’s health is worrying; it requires treatment."
+---旺达
+STRINGS.CHARACTERS.WANDA.ANNOUNCE_TALLBIRD_MOUNT_LOWHEALTH= "I ought to keep track of how much time the bird has left."
+---伍迪
+STRINGS.CHARACTERS.WOODIE.ANNOUNCE_TALLBIRD_MOUNT_LOWHEALTH= "I reckon I oughta patch this bird up."
+---沃利
+STRINGS.CHARACTERS.WARLY.ANNOUNCE_TALLBIRD_MOUNT_LOWHEALTH= "The bird deserves a fine feast to regain its strength"
+---薇格弗德
+STRINGS.CHARACTERS.WATHGRITHR.ANNOUNCE_TALLBIRD_MOUNT_LOWHEALTH= "My war mount is gravely wounded! We must retreät!"
+---薇洛
+STRINGS.CHARACTERS.WILLOW.ANNOUNCE_TALLBIRD_MOUNT_LOWHEALTH= "This dumb bird’s hurt bad! Let’s run for it!"
+---沃姆伍德
+STRINGS.CHARACTERS.WORMWOOD.ANNOUNCE_TALLBIRD_MOUNT_LOWHEALTH= "Peep-peep friend hurt lots."
+---麦斯威尔
+STRINGS.CHARACTERS.WAXWELL.ANNOUNCE_TALLBIRD_MOUNT_LOWHEALTH= "A few more strikes, and the Shadow King shall fall… to a bird."
+---韦伯
+STRINGS.CHARACTERS.WEBBER.ANNOUNCE_TALLBIRD_MOUNT_LOWHEALTH= "I think I heard the big bird’s bones crack!"
+---沃尔特
+STRINGS.CHARACTERS.WALTER.ANNOUNCE_TALLBIRD_MOUNT_LOWHEALTH= "The big bird should stop and rest, and have a little snack!"
+---沃特
+STRINGS.CHARACTERS.WURT.ANNOUNCE_TALLBIRD_MOUNT_LOWHEALTH= "Big bird hurt lots! Flurp."
+---WX-78
+STRINGS.CHARACTERS.WX78.ANNOUNCE_TALLBIRD_MOUNT_LOWHEALTH= "Warning: organic lifeform critically damaged."
+---沃拓克斯
+STRINGS.CHARACTERS.WORTOX.ANNOUNCE_TALLBIRD_MOUNT_LOWHEALTH= "Oh! The bird is injured! It requires healing"
+
+
+---给高脚鸟装其他鞍失败的台词
+---威尔逊
+STRINGS.CHARACTERS.GENERIC.ANNOUNCE_ISTALLBIRD_NOTSADDLE = "I ought to craft one more suitable for birds."
+---温蒂
+STRINGS.CHARACTERS.WENDY.ANNOUNCE_ISTALLBIRD_NOTSADDLE = "Not everything can be made do with."
+---薇诺娜
+STRINGS.CHARACTERS.WINONA.ANNOUNCE_ISTALLBIRD_NOTSADDLE= "This will end badly."
+---沃尔夫冈
+STRINGS.CHARACTERS.WOLFGANG.ANNOUNCE_ISTALLBIRD_NOTSADDLE= "This weight is not right for the big bird."
+---薇克巴顿
+STRINGS.CHARACTERS.WICKERBOTTOM.ANNOUNCE_ISTALLBIRD_NOTSADDLE= "This saddle is not meant for tallbirds."
+---旺达
+STRINGS.CHARACTERS.WANDA.ANNOUNCE_ISTALLBIRD_NOTSADDLE= "Ugh… I don’t know what it wants."
+---伍迪
+STRINGS.CHARACTERS.WOODIE.ANNOUNCE_ISTALLBIRD_NOTSADDLE= "A stinky bird’s mind is hard to figure out."
+---沃利
+STRINGS.CHARACTERS.WARLY.ANNOUNCE_ISTALLBIRD_NOTSADDLE= "I believe there’s a set better suited to its size."
+---薇格弗德
+STRINGS.CHARACTERS.WATHGRITHR.ANNOUNCE_ISTALLBIRD_NOTSADDLE= "This ill befits my mighty stëed."
+---薇洛
+STRINGS.CHARACTERS.WILLOW.ANNOUNCE_ISTALLBIRD_NOTSADDLE= "Quit bein’ so picky, you dummy!"
+---沃姆伍德
+STRINGS.CHARACTERS.WORMWOOD.ANNOUNCE_ISTALLBIRD_NOTSADDLE= "Peep-peep no like."
+---麦斯威尔
+STRINGS.CHARACTERS.WAXWELL.ANNOUNCE_ISTALLBIRD_NOTSADDLE= "How uncouth."
+---韦伯
+STRINGS.CHARACTERS.WEBBER.ANNOUNCE_ISTALLBIRD_NOTSADDLE= "The big bird doesn’t like this toy."
+---沃尔特
+STRINGS.CHARACTERS.WALTER.ANNOUNCE_ISTALLBIRD_NOTSADDLE= "We should get something nicer for my big bird friend."
+---沃特
+STRINGS.CHARACTERS.WURT.ANNOUNCE_ISTALLBIRD_NOTSADDLE= "No fun. Flurp."
+---WX-78
+STRINGS.CHARACTERS.WX78.ANNOUNCE_ISTALLBIRD_NOTSADDLE= "Error: incompatible."
+---沃拓克斯
+STRINGS.CHARACTERS.WORTOX.ANNOUNCE_ISTALLBIRD_NOTSADDLE= "My friend does not find this joke amusing."
+
+
+---给其他坐骑装高脚鸟鞍失败的台词
+---威尔逊
+STRINGS.CHARACTERS.GENERIC.ANNOUNCE_NOTTALLBIRD_ISSADDLE = "No, this is meant for tallbirds."
+---温蒂
+STRINGS.CHARACTERS.WENDY.ANNOUNCE_NOTTALLBIRD_ISSADDLE = "It rejects me."
+---薇诺娜
+STRINGS.CHARACTERS.WINONA.ANNOUNCE_NOTTALLBIRD_ISSADDLE= "This saddle isn't sized for it."
+---沃尔夫冈
+STRINGS.CHARACTERS.WOLFGANG.ANNOUNCE_NOTTALLBIRD_ISSADDLE= "Wolfgang feels it does not like this."
+---薇克巴顿
+STRINGS.CHARACTERS.WICKERBOTTOM.ANNOUNCE_NOTTALLBIRD_ISSADDLE= "This is a specialized saddle made for landfowl."
+---旺达
+STRINGS.CHARACTERS.WANDA.ANNOUNCE_NOTTALLBIRD_ISSADDLE= "What a waste of time."
+---伍迪
+STRINGS.CHARACTERS.WOODIE.ANNOUNCE_NOTTALLBIRD_ISSADDLE= "This’s for tying up pesky birds."
+---沃利
+STRINGS.CHARACTERS.WARLY.ANNOUNCE_NOTTALLBIRD_ISSADDLE= "The guest doesn’t care for this fit."
+---薇格弗德
+STRINGS.CHARACTERS.WATHGRITHR.ANNOUNCE_NOTTALLBIRD_ISSADDLE= "This beast is unwörthy of this saddle."
+---薇洛
+STRINGS.CHARACTERS.WILLOW.ANNOUNCE_NOTTALLBIRD_ISSADDLE= "This’s for catchin’ dumb big birds!"
+---沃姆伍德
+STRINGS.CHARACTERS.WORMWOOD.ANNOUNCE_NOTTALLBIRD_ISSADDLE= "For big peep-peep."
+---麦斯威尔
+STRINGS.CHARACTERS.WAXWELL.ANNOUNCE_NOTTALLBIRD_ISSADDLE= "It wasn't made for this creature. Don't waste your effort."
+---韦伯
+STRINGS.CHARACTERS.WEBBER.ANNOUNCE_NOTTALLBIRD_ISSADDLE= "That’s not how you play with this!"
+---沃尔特
+STRINGS.CHARACTERS.WALTER.ANNOUNCE_NOTTALLBIRD_ISSADDLE= "I think something must be wrong here."
+---沃特
+STRINGS.CHARACTERS.WURT.ANNOUNCE_NOTTALLBIRD_ISSADDLE= "Flurp. Fine, be that way!"
+---WX-78
+STRINGS.CHARACTERS.WX78.ANNOUNCE_NOTTALLBIRD_ISSADDLE= "Target error."
+---沃拓克斯
+STRINGS.CHARACTERS.WORTOX.ANNOUNCE_NOTTALLBIRD_ISSADDLE= "I shan’t give this to you!"
+
+
+---给野生高脚鸟装鞍失败的台词
+---威尔逊
+STRINGS.CHARACTERS.GENERIC.ANNOUNCE_TALLBIRD_ISWILD = "It'll peck my head off if I try this."
+---温蒂
+STRINGS.CHARACTERS.WENDY.ANNOUNCE_TALLBIRD_ISWILD = "Our friendship never even began."
+---薇诺娜
+STRINGS.CHARACTERS.WINONA.ANNOUNCE_TALLBIRD_ISWILD= "This thing will peck me to shreds."
+---沃尔夫冈
+STRINGS.CHARACTERS.WOLFGANG.ANNOUNCE_TALLBIRD_ISWILD= "Wolfgang never forces the animals."
+---薇克巴顿
+STRINGS.CHARACTERS.WICKERBOTTOM.ANNOUNCE_TALLBIRD_ISWILD= "It will provoke it—do not take the risk."
+---旺达
+STRINGS.CHARACTERS.WANDA.ANNOUNCE_TALLBIRD_ISWILD= "I suppose there’s no negotiating with it."
+---伍迪
+STRINGS.CHARACTERS.WOODIE.ANNOUNCE_TALLBIRD_ISWILD= "Why am I lookin’ for trouble?"
+---沃利
+STRINGS.CHARACTERS.WARLY.ANNOUNCE_TALLBIRD_ISWILD= "Might we build a little friendship first?"
+---薇格弗德
+STRINGS.CHARACTERS.WATHGRITHR.ANNOUNCE_TALLBIRD_ISWILD= "I mean no offence to this beast."
+---薇洛
+STRINGS.CHARACTERS.WILLOW.ANNOUNCE_TALLBIRD_ISWILD= "Maybe it’d cooperate if I roasted it first."
+---沃姆伍德
+STRINGS.CHARACTERS.WORMWOOD.ANNOUNCE_TALLBIRD_ISWILD= "Plant friend peep-peep friend?"
+---麦斯威尔
+STRINGS.CHARACTERS.WAXWELL.ANNOUNCE_TALLBIRD_ISWILD= "Not unless I have a death wish."
+---韦伯
+STRINGS.CHARACTERS.WEBBER.ANNOUNCE_TALLBIRD_ISWILD= "Maybe if we started when it was little…"
+---沃尔特
+STRINGS.CHARACTERS.WALTER.ANNOUNCE_TALLBIRD_ISWILD= "That doesn’t seem very nice…"
+---沃特
+STRINGS.CHARACTERS.WURT.ANNOUNCE_TALLBIRD_ISWILD= "Bird no trust merm. Flurp."
+---WX-78
+STRINGS.CHARACTERS.WX78.ANNOUNCE_TALLBIRD_ISWILD= "Warning: potential threat detected."
+---沃拓克斯
+STRINGS.CHARACTERS.WORTOX.ANNOUNCE_TALLBIRD_ISWILD= "It is not an easy target to bully."
