a64dab641a32235aaf6a6cd89e2a7cf8e62ab661
Guto <1017586494@qq.com>
2026-06-07 19:57:41 +0800

    smallbird and teenbird skin lua

--- scripts/prefabs/smallbird_skins.lua
+++ scripts/prefabs/smallbird_skins.lua
@@ -23,6 +23,90 @@ local skins = {
         skin_tags = { "SMALLBIRD", "MANRABBIIT" },
         build_name_override = "smallbirdskin_manrabbit",
     }),
+    CreatePrefabSkin("smallbird_pink", {
+        base_prefab = "smallbird",
+        type = "item",
+        rarity = "Elegant",
+        assets = {
+            Asset("ANIM", "anim/tallbirdskin_pink.zip"),
+        },
+        init_fn = init_fn,
+        clear_fn = clear_fn,
+        skin_tags = { "SMALLBIRD", "PINK" },
+        build_name_override = "tallbirdskin_pink",
+    }),
+    CreatePrefabSkin("smallbird_red", {
+        base_prefab = "smallbird",
+        type = "item",
+        rarity = "Elegant",
+        assets = {
+            Asset("ANIM", "anim/tallbirdskin_red.zip"),
+        },
+        init_fn = init_fn,
+        clear_fn = clear_fn,
+        skin_tags = { "SMALLBIRD", "RED" },
+        build_name_override = "tallbirdskin_red",
+    }),
+    CreatePrefabSkin("smallbird_blue", {
+        base_prefab = "smallbird",
+        type = "item",
+        rarity = "Elegant",
+        assets = {
+            Asset("ANIM", "anim/tallbirdskin_blue.zip"),
+        },
+        init_fn = init_fn,
+        clear_fn = clear_fn,
+        skin_tags = { "SMALLBIRD", "BLUE" },
+        build_name_override = "tallbirdskin_blue",
+    }),
+    CreatePrefabSkin("smallbird_green", {
+        base_prefab = "smallbird",
+        type = "item",
+        rarity = "Elegant",
+        assets = {
+            Asset("ANIM", "anim/tallbirdskin_green.zip"),
+        },
+        init_fn = init_fn,
+        clear_fn = clear_fn,
+        skin_tags = { "SMALLBIRD", "GREEN" },
+        build_name_override = "tallbirdskin_green",
+    }),
+    CreatePrefabSkin("smallbird_purple", {
+        base_prefab = "smallbird",
+        type = "item",
+        rarity = "Elegant",
+        assets = {
+            Asset("ANIM", "anim/tallbirdskin_purple.zip"),
+        },
+        init_fn = init_fn,
+        clear_fn = clear_fn,
+        skin_tags = { "SMALLBIRD", "PURPLE" },
+        build_name_override = "tallbirdskin_purple",
+    }),
+    CreatePrefabSkin("smallbird_brown", {
+        base_prefab = "smallbird",
+        type = "item",
+        rarity = "Elegant",
+        assets = {
+            Asset("ANIM", "anim/tallbirdskin_brown.zip"),
+        },
+        init_fn = init_fn,
+        clear_fn = clear_fn,
+        skin_tags = { "SMALLBIRD", "BROWN" },
+        build_name_override = "tallbirdskin_brown",
+    }),
+    CreatePrefabSkin("smallbird_snowman", {
+        base_prefab = "smallbird",
+        type = "item",
+        rarity = "Elegant",
+        assets = {
+            Asset("ANIM", "anim/tallbirdskin_snowman.zip"),
+        },
+        init_fn = init_fn,
+        clear_fn = clear_fn,
+        skin_tags = { "SMALLBIRD", "SNOWMAN" },
+        build_name_override = "tallbirdskin_snowman",
+    }),
 }
 
 return unpack(skins)
--- scripts/prefabs/teenbird_skins.lua
+++ scripts/prefabs/teenbird_skins.lua
@@ -23,6 +23,90 @@ local skins = {
         skin_tags = { "TEENBIRD", "MANRABBIT" },
         build_name_override = "tallbird_teenskin_manrabbit",
     }),
+    CreatePrefabSkin("teenbird_pink", {
+        base_prefab = "teenbird",
+        type = "item",
+        rarity = "Elegant",
+        assets = {
+            Asset("ANIM", "anim/tallbirdskin_pink.zip"),
+        },
+        init_fn = init_fn,
+        clear_fn = clear_fn,
+        skin_tags = { "TEENBIRD", "PINK" },
+        build_name_override = "tallbirdskin_pink",
+    }),
+    CreatePrefabSkin("teenbird_red", {
+        base_prefab = "teenbird",
+        type = "item",
+        rarity = "Elegant",
+        assets = {
+            Asset("ANIM", "anim/tallbirdskin_red.zip"),
+        },
+        init_fn = init_fn,
+        clear_fn = clear_fn,
+        skin_tags = { "TEENBIRD", "RED" },
+        build_name_override = "tallbirdskin_red",
+    }),
+    CreatePrefabSkin("teenbird_blue", {
+        base_prefab = "teenbird",
+        type = "item",
+        rarity = "Elegant",
+        assets = {
+            Asset("ANIM", "anim/tallbirdskin_blue.zip"),
+        },
+        init_fn = init_fn,
+        clear_fn = clear_fn,
+        skin_tags = { "TEENBIRD", "BLUE" },
+        build_name_override = "tallbirdskin_blue",
+    }),
+    CreatePrefabSkin("teenbird_green", {
+        base_prefab = "teenbird",
+        type = "item",
+        rarity = "Elegant",
+        assets = {
+            Asset("ANIM", "anim/tallbirdskin_green.zip"),
+        },
+        init_fn = init_fn,
+        clear_fn = clear_fn,
+        skin_tags = { "TEENBIRD", "GREEN" },
+        build_name_override = "tallbirdskin_green",
+    }),
+    CreatePrefabSkin("teenbird_purple", {
+        base_prefab = "teenbird",
+        type = "item",
+        rarity = "Elegant",
+        assets = {
+            Asset("ANIM", "anim/tallbirdskin_purple.zip"),
+        },
+        init_fn = init_fn,
+        clear_fn = clear_fn,
+        skin_tags = { "TEENBIRD", "PURPLE" },
+        build_name_override = "tallbirdskin_purple",
+    }),
+    CreatePrefabSkin("teenbird_brown", {
+        base_prefab = "teenbird",
+        type = "item",
+        rarity = "Elegant",
+        assets = {
+            Asset("ANIM", "anim/tallbirdskin_brown.zip"),
+        },
+        init_fn = init_fn,
+        clear_fn = clear_fn,
+        skin_tags = { "TEENBIRD", "BROWN" },
+        build_name_override = "tallbirdskin_brown",
+    }),
+    CreatePrefabSkin("teenbird_snowman", {
+        base_prefab = "teenbird",
+        type = "item",
+        rarity = "Elegant",
+        assets = {
+            Asset("ANIM", "anim/tallbirdskin_snowman.zip"),
+        },
+        init_fn = init_fn,
+        clear_fn = clear_fn,
+        skin_tags = { "TEENBIRD", "SNOWMAN" },
+        build_name_override = "tallbirdskin_snowman",
+    }),
 }
 
 return unpack(skins)
