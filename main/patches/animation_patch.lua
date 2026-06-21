c03e0c9452bd6b9e2ed4e988bf9cb41bd1f144ca
over-dragon <over_dragon@163.com>
2026-06-21 12:13:52 +0800

    exchange jump

--- main/patches/animation_patch.lua
+++ main/patches/animation_patch.lua
@@ -30,6 +30,9 @@ local function ReplaceAttackAnim(inst, anim)
             return "atkleg"
         end
     end
+    if inst and inst:HasTag("tallbird") then
+        print(anim)
+    end
     return anim
 end
 
