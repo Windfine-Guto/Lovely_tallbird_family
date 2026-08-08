0b97805caff46711eabba5d9f26e1040ecdf0d61
Guto <1017586494@qq.com>
2026-08-09 00:05:09 +0800

    modinfo

--- modinfo.lua
+++ modinfo.lua
@@ -1,7 +1,7 @@
 local modid = 'lovely_tallbird_family'
 local cur = (locale == 'zh' or locale == 'zhr') and 'zh' or 'en'
 
-version = '26.08.08.2'
+version = '26.08.08.9'
 author = 'over_dragon、Guto'
 
 forumthread = ''
